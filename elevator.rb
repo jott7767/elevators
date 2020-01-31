f62d9be010a82eb5ece25dd9acec3e796daa6ee7
Jordan Ott <ottjw@yahoo.com>
2020-01-30 23:21:17 -0700

    pick elevator, elevator moves

--- elevator.rb
+++ elevator.rb
@@ -6,16 +6,31 @@ class Elevator
     @passengers = []
     @current_direction = nil
     @current_floor = rand(1..floors)
+    @elevator_requests = []
   end
 
   attr_reader :floors
   attr_accessor :current_floor, :current_direction
 
-  def passengers
-
+  def move
+    current_direction = nil if empty?
+    return unless current_direction
+    case current_direction
+    when "up"
+      current_floor += 1
+    when "down"
+      current_floor -= 1
+    else
+      return
+    end
+    check_floor
   end
 
-  def busy?
+  def passengers
+    @passengers
+  end
 
+  def empty?
+    @passengers.empty? && @elevator_requests.empty?
   end
 end
--- elevator_system.rb
+++ elevator_system.rb
@@ -12,11 +12,22 @@ class ElevatorSystem
   end
 
   def elevator_request(floor, direction)
-
+    pick_best_elevator.elevator_requests << ElevatorRequest.new(floor, direction)
   end
 
   def time_passed
     @elevators.each(&:move)
+  end
+
+  def pick_best_elevator
+    if all_busy?
+      closest
+    else
+      closest
+  end
+
+  def all_busy?
+    !@elevators.all?(&:empty)
   end
 
 end
